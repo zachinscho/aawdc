57f731ede445fc1ee782234ad2c7e4912263e260
Daniel Rogers <drogers98@gmail.com>
2015-09-03 20:37:29 -0600

    first round structure change homepage

--- sites/all/themes/aawdc_desktop/templates/page--front.tpl.php
+++ sites/all/themes/aawdc_desktop/templates/page--front.tpl.php
@@ -2,39 +2,25 @@
 <div role="document" class="page">
 
   <!--.l-header -->
-  <header role="banner" class="l-header">
-
-    <?php if ($top_bar): ?>
-      <!--.top-bar -->
-      <?php if ($top_bar_classes): ?>
-        <div class="<?php print $top_bar_classes; ?>">
-      <?php endif; ?>
-      <nav class="top-bar"<?php print $top_bar_options; ?>>
-        <ul class="title-area">
-          <li class="name"><a href="<?php print $front_page; ?>"></a></li>
-          <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
-        </ul>
-        <section class="top-bar-section">
+  <header class="l-header">
+  	
+  	<a href="#" class="logo"></a>
+  	
+         <nav>
           <?php if ($top_bar_main_menu) :?>
             <?php print $top_bar_main_menu; ?>
           <?php endif; ?>
           <?php if ($top_bar_secondary_menu) :?>
             <?php print $top_bar_secondary_menu; ?>
           <?php endif; ?>
-        </section>
-      </nav>
-      <?php if ($top_bar_classes): ?>
-        </div>
-      <?php endif; ?>
-      <!--/.top-bar -->
-    <?php endif; ?>
-
+        </nav>
+        <?php print render($page['header']); ?>
 
     <?php if (!empty($page['header'])): ?>
       <!--.l-header-region -->
       <section class="l-header-region row">
         <div class="large-12 columns">
-          <?php print render($page['header']); ?>
+          
         </div>
       </section>
       <!--/.l-header-region -->
@@ -43,13 +29,6 @@
   </header>
   <!--/.l-header -->
   
-      <!--.l-BC-->
-    <section class="l-BC row">
-      <div class="large-12 columns">
-        <?php if ($breadcrumb): print $breadcrumb; endif; ?>
-      </div>
-    </section>
-    <!--/.l-BC -->
 
   <?php if (!empty($page['featured'])): ?>
     <!--.featured -->
@@ -81,39 +60,130 @@
     <!--/.l-help -->
   <?php endif; ?>
   
-   
 
-
-  
-
-  <main role="main" class="row l-main">
-homepage
-  </main>
+	<div class="imageHero">
+		<section class="row">
+			<div class="large-12 columns">
+        		Committed to you
+      		</div>
+    	</section>
+	</div>
+	
+	<div class="calloutTryp">
+	  	<section class="row">
+		  	<div class="large-4 columns negative">
+	        	<span></span>
+	        	<h3>Take the next career step</h3>
+	      	</div>
+	      	<div class="large-4 columns negative">
+	        	<span></span>
+	        	<h3>Build your workforce</h3>
+	      	</div>
+	      	<div class="large-4 columns negative">
+	        	<span></span>
+	        	<h3>Get Involved</h3>
+	      	</div> 	
+	  	</section>
+	</div>
+	
+	<div class="calloutHero">
+		<section class="row">
+			<div class="large-12 columns">
+        		Our vision, mission, and core values are at the heart of the way we work to help you achieve success.  Our committed staff and Boards of Directors want to help you to achieve the goals for your business or career success.
+      		</div>
+    	</section>
+	</div>
+	
+	<div class="mainWrapper">
+	  <div class="row">
+	  	<div class="large-4 columns negative">
+        		<h3>10,000</h3> 
+        		Job seekers Trained.
+       	</div>
+      	
+      	<div class="large-4 columns negative">
+        		<h3>10,000</h3> 
+        		Job seekers Trained.
+      	</div>
+      	
+      	<div class="large-4 columns negative">
+        		<h3>10,000</h3> 
+        		Job seekers Trained.
+      	</div>
+	  </div>
+	  
+	  <div class="row">
+	  	<div class="large-9 columns negative">
+		  	<span>Blog</span>
+		  	<h3>Title Here</h3>
+        	<p>Our vision, mission, and core values are at the heart of the way we work to help you achieve success.  Our committed staff and Boards of Directors want to help you to achieve the goals for your business or career success.</p>
+      	</div>
+      	
+      	<div class="large-3 columns negative">
+        	<span>What's New</span>
+		  	<h3>Title Here</h3>
+        	<p>Our vision, mission, and core values are at the heart of the.</p>
+      	</div>
+      	
+	  </div>
+	  
+	  <div class="row">
+	  	<div class="large-9 columns negative">
+        	<span>Success Stories</span>
+		  	<h3>Title Here</h3>
+        	<p>Our vision, mission, and core values are at the heart of the way we work to help you achieve success.  Our committed staff and Boards of Directors want to help you to achieve the goals for your business or career success.</p>
+      	</div>
+      	
+      	<div class="large-3 columns negative">
+        	<span>Hot Jobs</span>
+		  	<h3>Title Here</h3>
+        	<p>Our vision, mission, and core values are at the heart of the way we work to.</p>
+      	</div>
+      	
+	  </div>
+	  
+	</div>
 
   <!--/.l-main-->
-
-
-    <!--.l-footer-->
-    <footer class="l-footer panel" role="contentinfo">
-    <div class="row">
-          
-        <div class="large-9 columns">
-	      <p>7493 Baltimore-Annapolis Blvd. Glen Burnie, MD 21061 | <a href="tel://410-766-5212">410-766-5212</a></p>
-          <p class="copyright"><a href="http://aawdc.org/">&copy; <?php print date('Y') . ' ' . "Anne Arundel Workforce Development Corporation" . ' ' . t('All rights reserved.'); ?></a> AAWDC YouthWorks programs are equal opportunity, affirmative action, Title IX and ADA Title 504 compliant. Assistive devices and alternative materials for services are available with advanced request.</p>
-        </div>
-       
-    </div>
-    
-     <div class="socialFooter">
-	        <div class="footerInner">
-		        <a href="https://twitter.com/aawdc"><i class="icon icomoon icon-Twitter" aria-hidden="true"></i></a>
-		        <a href="https://www.facebook.com/pages/AAWDCs-Youth-Employment-Services-YES-Center/344111735673607?fref=nf"><i class="icon icomoon icon-Facebook" aria-hidden="true"></i></a>
-		        <a href="https://www.linkedin.com/company/1447261?trk=tyah"><i class="icon icomoon icon-Linked_In" aria-hidden="true"></i></a>
-	        </div>
-	 </div>
-
+	<footer class="topFooter">
+	  	<div class="row">
+			  <div class="large-4 columns">
+				  <h3>Learn More</h3>
+			  </div>
+			  <div class="large-4 columns">
+				  <h3>Get Involved</h3>
+			  </div>
+			  <div class="large-4 columns">
+				  <h3>Stay In Touch</h3>
+				 <div class="footerInner">
+			        <a href="https://twitter.com/aawdc"><i class="icon icomoon icon-Twitter" aria-hidden="true"></i></a>
+			        <a href="https://www.facebook.com/pages/AAWDCs-Youth-Employment-Services-YES-Center/344111735673607?fref=nf"><i class="icon icomoon icon-Facebook" aria-hidden="true"></i></a>
+			        <a href="https://www.linkedin.com/company/1447261?trk=tyah"><i class="icon icomoon icon-Linked_In" aria-hidden="true"></i></a>
+		        </div>
+			  </div>
+		  </div>
+	</footer>	  
+	  
+    <footer class="middleFooter">  
+	    <div class="row">
+	          <div class="large-4 columns">
+		          <span>Get in touch</span>
+		          <p><a href="tel://410-766-5212">410-766-5212</a><br/>
+		          7493 Baltimore-Annapolis Blvd. Glen Burnie, MD 21061</p>
+	          </div>
+			  <div class="large-8 columns">
+		           <p class="copyright"><a href="http://aawdc.org/">&copy; <?php print date('Y') . ' ' . "Anne Arundel Workforce Development Corporation" . ' ' . t('All rights reserved.'); ?></a> AAWDC YouthWorks programs are equal opportunity, affirmative action, Title IX and ADA Title 504 compliant. Assistive devices and alternative materials for services are available with advanced request.</p>
+	          </div>  
+	    </div>
     </footer>
-    <!--/.footer-->
+    
+    <footer class="bottomFooter">  
+	    <div class="row">
+	          <a href="#" class="MWE"></a>
+	          <a href="#" class="MJN"></a>
+	    </div>
+    </footer>
+    
 
   <?php if ($messages && $zurb_foundation_messages_modal): print $messages; endif; ?>
 </div>
