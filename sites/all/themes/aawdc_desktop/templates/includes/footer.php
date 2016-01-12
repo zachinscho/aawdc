ee14c801322099728b5547feda842feee288f6e7
Daniel Rogers <drogers98@gmail.com>
2016-01-11 20:22:39 -0700

    footer block

--- sites/all/themes/aawdc_desktop/templates/includes/footer.php
+++ sites/all/themes/aawdc_desktop/templates/includes/footer.php
@@ -17,9 +17,7 @@
 	    <div class="row">
 	          <div class="large-4 columns getInTouch">
 		          <span class="smallLabel">Get in touch</span>
-		          <a href="tel://410-987-3890">410-987-3890</a><br/>
-		          <p>AAWDC | 401 Headquarters Drive <br/>
-					 Suite 208 | Millersville, MD 21108</p></p>
+		          <?php print render($page['footerFour']); ?>
 	          </div>
 			  <div class="large-8 columns legal">
 		           <p class="copyright"><a href="http://aawdc.org/">&copy; <?php print date('Y') . ' ' . "Anne Arundel Workforce Development Corporation" . ' ' . t('All rights reserved.'); ?></a> AAWDC YouthWorks programs are equal opportunity, affirmative action, Title IX and ADA Title 504 compliant. Assistive devices and alternative materials for services are available with advanced request.</p>
