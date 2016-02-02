08cfee35bd80a4c5292ab38d9aab76930fb00a10
Dan Rogers <dan.rogers@idfive.com>
2016-02-02 06:17:49 -0700

    remove second nav

--- sites/all/themes/aawdc_desktop/templates/includes/navigation.php
+++ sites/all/themes/aawdc_desktop/templates/includes/navigation.php
@@ -1,14 +1,11 @@
  <!--.l-header -->
   <header class="l-header">
-  	
+
   	<a href="<?php print $front_page; ?>" class="logo"></a>
-  	
+
          <nav>
           <?php if ($top_bar_main_menu) :?>
             <?php print $top_bar_main_menu; ?>
-          <?php endif; ?>
-          <?php if ($top_bar_secondary_menu) :?>
-            <?php print $top_bar_secondary_menu; ?>
           <?php endif; ?>
         </nav>
         <?php print render($page['header']); ?>
@@ -17,7 +14,7 @@
       <!--.l-header-region -->
       <section class="l-header-region row">
         <div class="large-12 columns">
-          
+
         </div>
       </section>
       <!--/.l-header-region -->
